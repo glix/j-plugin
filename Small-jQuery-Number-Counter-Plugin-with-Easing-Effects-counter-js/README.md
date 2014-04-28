8a9e92a35ac88a0b85f51318a17e66bdf15728e8
Danny Glix <danny@dannyglix.com>
2014-04-28 09:32:53 +0530

    Added Number Counter Plugin

---
+++ Small-jQuery-Number-Counter-Plugin-with-Easing-Effects-counter-js/README.md
@@ -0,0 +1,145 @@
+# Counter.js
+
+A _simple_ and _small_ number counter in JavaScript as a
+[jQuery](http://jquery.com) plugin.
+It counts to or down to the number set on an element using an easing effect.
+
+You can configure certain options such as a duration, easing effect, auto start,
+start at count, placeholder (for use with auto start) and it supports callbacks.
+
+## Example
+
+The index.html file contains a working example of the counter.
+
+It's also live at
+[tombruijn.github.io/counter.js](http://tombruijn.github.io/counter.js).
+
+## Usage
+
+__Simplest implementation:__
+
+```html
+<!-- HTML -->
+<div id="counter">1024</div>
+```
+
+```javascript
+// JavaScript
+$("#counter").counter();
+```
+
+__With some more options:__
+
+```javascript
+// JavaScript
+$("#counter").counter({
+  autoStart: false,           // true/false, default: true
+  duration: 5000,             // milliseconds, default: 1500
+  countFrom: 10,              // start counting at this number, default: 0
+  countTo: 30,                // count to this number, default: 0
+  runOnce: true,              // only run the counter once, default: false
+  placeholder: "?",           // replace the number with this before counting,
+                              // most useful with autoStart: false. default: undefined
+  easing: "easeOutCubic",     // see http://gsgd.co.uk/sandbox/jquery/easing
+                              // for all available effects, see visual examples:
+                              // http://easings.net
+                              // default: "easeOutQuad"
+  onStart: function() {},     // callback on start of the counting
+  onComplete: function() {},  // callback on completion of the counting
+  numberFormatter:            // function used to format the displayed numbers.
+    function(number) {
+      return "$ " + number;
+    }
+});
+```
+
+Counter.js supports counting up and counting down.
+If either countFrom or countTo is not set it will try to determine which value
+you meant by its innerHTML of the element and the countFrom and countTo options.
+You can also not set an innerHTML value on the element and use both options.
+
+## Installation
+
+If you're just interested in just using the script then copy the
+`js/counter.min.js` file in your project.
+Then add the libraries as described in `Requirements` below.
+
+## Requirements
+
+You will need two libraries, jQuery (works with v2 and up, v1.64 and up
+(I use as little jQuery as possible)) and the jQuery easing plugin (v1.3)
+which is optional. You only need jQuery easing when you need a different
+counting effect than swing.
+
+Make sure to add these files to your project in this order:
+
+```javascript
+jquery.js        // Download from: http://jquery.com
+jquery.easing.js // Download from: http://gsgd.co.uk/sandbox/jquery/easing/
+counter.js       // Is located in this repository under `js/counter.js`
+                 // A minified version is also available.
+```
+
+_(I don't recommend you using the `js/jquery.js` and `js/jquery.easing.js` files
+that are available in this repository in your project.
+Instead download the latest ones from [jquery.com](http://jquery.com/) and
+[gsgd.co.uk (jQuery.easing)](http://gsgd.co.uk/sandbox/jquery/easing/).)_
+
+## Contributing
+
+When you find a bug, please report it in the issue tracker on
+[GitHub](https://github.com/tombruijn/counter.js/issues).
+
+If you're missing a feature, want to help make this project better or just
+want to modify it?
+Fork the project on [GitHub](https://github.com/tombruijn/counter.js)
+(and send in a Pull Request).
+
+Do you need help to start the development? Here's how:
+
+### Requirements
+
+For development you will need at least the CoffeeScript compiler.
+The JavaScript minifier is not required, but it can't hurt to run your specs
+with the minified version as well.
+
+I use the [coffee-script](https://github.com/jashkenas/coffee-script) package in
+[Node.js](http://nodejs.org) for compilation.
+
+For minification I use [UglifyJS2](https://github.com/mishoo/UglifyJS2).
+
+### Compilation
+
+Compiled and minified javascript should **not** be committed in pull requests.
+This will be done with every new release instead.
+
+Use the bundled `Makefile` to compile and minify the original coffeescript file.
+It's as simple as running `make` inside the project's directory.
+Do make sure that all the required Node.js packages are installed.
+
+For development purposes I recommend using the watch feature on the
+`coffee-script` package so it will compile on every file change.
+
+`coffee --watch --compile ./js/counter.coffee`
+
+### Testing
+
+This project is tested with [Jasmine](http://jasmine.github.io/) 2.0.
+
+In the spec directory you can find all the tests for this project.
+The spec suite expects a compiled `counter.js` file to be available in the
+`js` directory, so make sure you have compiled it before testing.
+
+The project uses the Jasmine Ruby gem so I don't have to copy Jasmine into this
+project.
+
+Start the Jasmine server with `rake jasmine` and visit the returned url.
+
+## Code status
+
+[![Build Status](https://travis-ci.org/tombruijn/counter.js.png)](https://travis-ci.org/tombruijn/counter.js)
+
+## License
+
+Counter.js released under the MIT License. See the bundled LICENSE file for
+details.
---
+++ Small-jQuery-Number-Counter-Plugin-with-Easing-Effects-counter-js/spec/javascripts/counter_spec.js
@@ -0,0 +1,192 @@
+describe("Counter.js", function() {
+  var element, counter;
+
+  beforeEach(function() {
+    element = $("<div>");
+    counter = element.counter({autoStart: false}).data("plugin_counter");
+  });
+
+  describe("counter()", function(){
+    it("should set plugin data attribute on element", function() {
+      expect(typeof $("<div>").data("plugin_counter")).toEqual("undefined");
+      expect(typeof counter).toEqual("object");
+    });
+
+    it("should execute start when a string with start is given", function() {
+      expect(counter.running).toBeUndefined();
+      element.counter("start");
+      expect(counter.running).toBe(true);
+    });
+
+    it("should merge set options", function(){
+      var onStart = function() { };
+      var onComplete = function() { };
+      var element = $("<div>");
+      element.counter({
+        autoStart: false,
+        duration: 10,
+        countFrom: 1,
+        countTo: 2,
+        placeholder: "?",
+        easing: "linear",
+        onStart: onStart,
+        onComplete: onComplete
+      });
+      options = element.data("plugin_counter").options
+      expect(options.autoStart).toBe(false);
+      expect(options.duration).toBe(10);
+      expect(options.countFrom).toBe(1);
+      expect(options.countTo).toBe(2);
+      expect(options.placeholder).toBe("?");
+      expect(options.easing).toBe("linear");
+      expect(options.onStart).toBe(onStart);
+      expect(options.onComplete).toBe(onComplete);
+    });
+
+    it("should set defaults for undefined options", function(){
+      var element = $("<div>");
+      element.counter({ countFrom: 0, countTo: 1 });
+      options = element.data("plugin_counter").options
+      expect(options.autoStart).toBe(true);
+      expect(options.duration).toBe(1500);
+      expect(options.countFrom).toBe(0);
+      expect(options.countTo).toBe(1);
+      expect(options.easing).toBe("easeOutQuad");
+    });
+
+    describe("element value", function(){
+      it("should use the element value for countTo if larger than countFrom", function(){
+        var element = $("<div>1</div>");
+        element.counter({ countFrom: 0 });
+        options = element.data("plugin_counter").options
+        expect(options.countFrom).toBe(0);
+        expect(options.countTo).toBe(1);
+      });
+
+      it("should use the element value for countFrom if larger than countTo", function(){
+        var element = $("<div>1</div>");
+        element.counter({ countTo: 2 });
+        options = element.data("plugin_counter").options
+        expect(options.countFrom).toBe(1);
+        expect(options.countTo).toBe(2);
+      });
+    });
+
+    describe("placeholder", function(){
+      it("should set the placeholder in the HTML", function(){
+        var element = $("<div>1</div>");
+        element.counter({ autoStart: false, countFrom: 0, placeholder: "?" });
+        expect(element[0].innerHTML).toBe("?");
+      });
+    });
+
+    describe("numberFormatter", function(){
+      it("should set format on element", function(){
+        var element = $("<div>1</div>");
+        element.counter({
+          autoStart: true,
+          countTo: 10,
+          duration: 0,
+          numberFormatter: function(number){
+            return "$ " + number;
+          }
+        });
+        expect(element[0].innerHTML).toBe("$ 10");
+      });
+    });
+  });
+
+  describe("start()", function(){
+    var options = null;
+
+    beforeEach(function() {
+      element = $("<div>0</div>");
+      options = {
+        autoStart: false,
+        duration: 1000,
+        countFrom: 0,
+        countTo: 1000,
+        onStart: function() { },
+        onComplete: function() { }
+      };
+      spyOn(options, "onStart");
+      spyOn(options, "onComplete");
+      counter = element.counter(options).data("plugin_counter");
+      jQuery.fx.off = true;
+    });
+
+    it("should increment the element's value", function(){
+      expect(element[0].innerHTML).toBe("0");
+      counter.start();
+      expect(element[0].innerHTML).toBe("1000");
+    });
+
+    it("should prevent calls when already running", function(){
+      jQuery.fx.off = false;
+      expect(options.onStart).not.toHaveBeenCalled();
+      expect(options.onComplete).not.toHaveBeenCalled();
+      expect(counter.running).toBeUndefined();
+
+      counter.start();
+      expect(counter.running).toBe(true);
+      counter.start();
+      counter.start();
+
+      expect(options.onStart.calls.count()).toEqual(1);
+      expect(options.onComplete.calls.count()).toEqual(0);
+    });
+
+    it("should trigger callbacks", function(){
+      expect(options.onStart).not.toHaveBeenCalled();
+      expect(options.onComplete).not.toHaveBeenCalled();
+      counter.start();
+      expect(options.onStart).toHaveBeenCalled();
+      expect(options.onComplete).toHaveBeenCalled();
+    });
+
+    describe("runOnce option = true", function(){
+      beforeEach(function() {
+        element = $("<div>0</div>");
+        options = {
+          autoStart: false,
+          duration: 0,
+          countFrom: 0,
+          countTo: 1000,
+          runOnce: true,
+          onStart: function() { },
+          onComplete: function() { }
+        };
+        spyOn(options, "onStart");
+        spyOn(options, "onComplete");
+        counter = element.counter(options).data("plugin_counter");
+        jQuery.fx.off = false;
+      });
+
+      it("should only run once", function(){
+        expect(options.onStart).not.toHaveBeenCalled();
+        expect(options.onComplete).not.toHaveBeenCalled();
+        expect(counter.running).toBeUndefined();
+
+        counter.start();
+        expect(options.onStart.calls.count()).toEqual(1);
+        expect(options.onComplete.calls.count()).toEqual(1);
+
+        counter.start();
+        expect(options.onStart.calls.count()).toEqual(1);
+        expect(options.onComplete.calls.count()).toEqual(1);
+      });
+    });
+  });
+
+  describe("setNumber()", function(){
+    it("should modify the element's innerHTML", function(){
+      counter.setNumber(1);
+      expect(element[0].innerHTML).toBe("1");
+    });
+
+    it("should round the value", function(){
+      counter.setNumber(3.14);
+      expect(element[0].innerHTML).toBe("3");
+    });
+  });
+});
