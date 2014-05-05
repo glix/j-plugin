5977ca729301e25eb61c5d8c38b578d7a551fd43
Danny Glix <danny@dannyglix.com>
2014-05-05 08:17:39 +0530

    Added Display Social Networking

---
+++ jQuery-Plugin-For-Displaying-Social-Network-Feeds-Social-Feed/README.md
@@ -0,0 +1,93 @@
+Social-feed
+===========
+The jQuery plugin which shows a user feed from the most popular social networks.<br/> 
+Currently are supported: <a href="http://facebook.com">Facebook</a>, <a href="http://instagram.com">Instagram</a>, <a href="http://vk.com">VK</a>, <a href="http://plus.google.com">Google+</a>, (<a href="http://twitter.com">Twitter</a> is currently not supported)
+<hr>
+If you use this plugin, please <a href="mailto:pavel@kucherbaev.com">write me a short message</a> with a link to the project where you embed the plugin, and some features you want to have implemented here. It will help me to stay focused on the important issues and see the global picture!
+<br/><strong>It is not mandatory, but I really appreciate it!</strong>
+<p>
+<strong>Attention:</strong> After Twitter has prohibided an anonymous fetching of tweets - this function is currently not supported. Wait an update with a fix.
+</p>
+<hr>
+<h4><a href="http://pavelk2.github.io/social-feed/" target="_blank">Online demo is here</a><h4>
+
+<img src="http://habrastorage.org/storage2/bc3/834/e4d/bc3834e4dd952f22b470830d7dc1096c.png" />
+<hr>
+With previews:<br/>
+<img src="http://habrastorage.org/storage2/2ee/a85/fcf/2eea85fcf3c76efb328b0b2d9e8df7ad.png" />
+<hr>
+<h4>Getting started</h4>
+Connect css:
+
+        <!-- Social-feed css -->
+        <link href="css/jquery.socialfeed.css" rel="stylesheet" type="text/css">
+        <!-- font-awesome for social network icons -->
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+Create a container for your feed:
+
+        <div class="social-feed-container"></div>
+
+Connect js:
+
+        <!-- jQuery -->
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    
+        <!-- doT.js for rendering templates and moment.js for showing time ago -->
+        <script src="dependencies/doT.min.js"></script>
+        <script src="dependencies/moment.min.js"></script>
+    
+        <!-- Social-feed js -->
+        <script src="js/jquery.socialfeed.js"></script>
+
+Initialize the social-feed plugin:
+
+        <script>
+         $('.social-feed-container').socialfeed({
+                    //FACEBOOK--------------------
+                    facebook:{
+                        accounts:['teslamotors'], //usernames or id
+                        limit:2,
+                        token:'YOUR_FACEBOOK_ACCESS_TOKEN' //you can also create an app and put  here your 'APP ID|APP SECRET' - it is easier but not safe
+                    },
+                    //VK--------------------
+                    vk:{
+                        accounts:[125936523], //id for users and -id (with minus) for groups 
+                        limit:2,
+                        source:'all'
+                    },
+                    //GOOGLEPLUS-------------------
+                    google:{
+                         access_token: 'YOUR_GOOGLE_PLUS_TOKEN',
+                         accounts: ['111435337725041517235','114461178896543099856','+TeslaMotors'],
+                         limit: 2
+                     },
+                    //INSTAGRAM---------------------
+                    instagram:{
+                        accounts:[297604134], //userid
+                        client_id:'YOUR_INSTAGRAM_APP_CLIENT_ID',
+                        limit:2
+                    },
+                    //GENERAL SETTINGS--------------
+                    length:130,
+                    show_media:true,
+                    callback: function(){
+                        console.log('all posts are collected');
+                    }
+                });
+        </script>
+
+When you run the plugin, make sure that you have your <strong>webserver running</strong>
+
+If you want to change the layout of the feed, you can do it in the <em>template.html</em> file.
+<br/>
+If you don't need to show the feed from all the supported social networks, put the credentials only for those you need.
+
+<h4>Dependencies:</h4>
+-  http://fontawesome.io/ - for displaying icons of social networks
+-  http://momentjs.com/ - for displaying time ago
+-  http://olado.github.io/doT/ - for rendering templates
+
+<strong>Attention!</strong> The current version of the plugin does not fully support IE browser. Please follow the process of solving issues for IE.
+
+
---
+++ jQuery-Plugin-For-Displaying-Social-Network-Feeds-Social-Feed/js/jquery.socialfeed.js
@@ -0,0 +1,415 @@
+if ( typeof Object.create !== 'function' ) {
+    Object.create = function( obj ) {
+        function F() {};
+        F.prototype = obj;
+        return new F();
+    };
+}
+;
+(function( $, window, document, undefined ) {
+    /*var socialFeed = {
+    };
+    $.fn.socialfeed = function( options ) {
+               return this.each(function() {
+                       var feed = Object.create( socialFeed );
+                       
+                       feed( options, this );
+
+                       $.data( this, 'socialfeed', feed );
+               });
+   };*/
+$.fn.socialfeed = function(options)
+{
+    var defaults = {
+            plugin_folder: '', // a folder in which the plugin is located (with a slash in the end)
+            template: 'template.html', // a path to the tamplate file
+            show_media: false, //show images of attachments if available
+            cookies: false, //if true then twitter results will be saved in cookies, to fetch them if 150 requests/hour is over.
+            length: 500 // maximum length of post message shown
+        };
+    //---------------------------------------------------------------------------------
+    var options = $.extend(defaults, options),container = $(this),template,loaded = []; 
+    container.empty().css('display', 'inline-block');
+    //---------------------------------------------------------------------------------
+    // Initiate function
+    return getAllData();
+    //---------------------------------------------------------------------------------
+    //This function performs consequent data loading from all of the sources by calling corresponding functions
+    function getAllData(){
+        if (options.facebook) {
+            loaded['fb']=0;
+            $.each(options.facebook.accounts,function(){
+                loaded['fb']++;
+                getFacebookData(this);
+            });
+            
+        }
+        if (options.twitter) {
+            loaded['tw']=0;
+            $.each(options.twitter.accounts,function(){
+                loaded['tw']++;
+                getTwitterData(this);
+            });
+        }
+        if (options.vk) {
+            loaded['vk']=0;
+            $.each(options.vk.accounts,function(){
+                loaded['vk']++;
+                getVKData(this);
+            });
+        }
+        if (options.google){
+            loaded['google']=0;
+            $.each(options.google.accounts,function(){
+                loaded['google']++;
+                getGoogleplusData(this);
+            });
+        }
+        if (options.instagram){
+            loaded['igm']=0;
+            $.each(options.instagram.accounts,function(){
+                loaded['igm']++;
+                getInstagramData(this);
+            });
+            
+        }
+    }
+    function fireCallback(){
+        var fire = true;
+        $.each(Object.keys(loaded),function(){
+            if (loaded[this] > 0)
+                fire = false;
+        });
+        if (fire && options.callback)
+            options.callback();
+    }
+    function getFacebookData(account){
+        var limit = 'limit=' + options.facebook.limit,
+        query_extention = '&access_token=' + options.facebook.token + '&callback=?',
+        fb_graph = 'https://graph.facebook.com/',
+        feed_json = fb_graph + account + '/feed?' + limit + query_extention; 
+        $.get(feed_json,function(json){
+
+           $.each(json.data,function(){
+            var element = this,
+            post = {};
+            if (element.message || element.story){
+                var text = element.story, url = 'http://facebook.com/' + element.from.id
+                if (element.message)
+                    text = element.message;                            
+                if (element.link)
+                    url = element.link;   
+                if (options.show_media){
+                    if (element.picture){
+                        post.attachment = '<img class="attachment" src="' + element.picture.replace('_s.', '_b.') + '" />';
+                    }
+                }
+                post.dt_create = moment(element.created_time);
+                post.author_link = 'http://facebook.com/' + element.from.id;
+                post.author_picture = fb_graph + element.from.id + '/picture';
+                post.post_url = url;
+                post.author_name = element.from.name;
+                post.message = text;
+                post.description = (element.description) ? element.description : '';
+                post.link = url;
+                post.social_network = 'facebook';
+                getTemplate(post, json.data[json.data.length-1] == element);
+            }
+        });
+},'json');
+
+}
+function getGoogleplusData(account){
+    var content = 'https://www.googleapis.com/plus/v1/people/' + account + '/activities/public?key=' + options.google.access_token + '&maxResults=' + options.google.limit;
+    $.ajax({
+     url : content,
+     dataType : 'jsonp',
+     success:function(json){     
+
+       $.each(json.items, function(){              
+        var post = {},
+        element = this;     
+        post.attachment = '';      
+        post.description = '';                                                 
+        post.dt_create = moment(element.published);
+        post.author_link = element.actor.url;
+        post.author_picture = element.actor.image.url;
+        post.post_url = element.url;
+        post.author_name = element.actor.displayName;
+        if(options.show_media === true){
+            if(element.verb === 'share' && element.object.content === ""){                                                                                                          
+                $.each(element.object.attachments, function(){
+                    share = this;                                    
+                    post.attachment = '<img width="50%" src="' + share.image.url + '"/>';                                
+                });                                                          
+            }          
+            if(element.verb === 'share'){                                                  
+                $.each(element.object.attachments, function(){  
+                    if(this.thumbnails.length > 1){
+                        $.each(this.thumbnails, function(){                                                                                                                                      
+                            post.attachment += '<img src="' + this.image.url + '"/>';                                                                                                                                                                                                                                                       
+                        });   
+                    }
+                    else{                                    
+                        post.attachment =  '<img src="' + this.image.url + '"/>';
+                    }        
+                });
+            }
+            else{
+                if(element.object.attachments !== undefined){
+                    $.each(element.object.attachments, function(){
+                        var image = '';
+                        if (this.image){
+                            image=  this.image.url;
+                        }else{
+                            if (this.objectType=='album'){
+                                if (this.thumbnails.length>0)
+                                    image=this.thumbnails[0].image.url;
+                            }
+                        }
+                        post.attachment = '<img width="50%;" src="' + image + '"/>';
+                    });
+                }
+            }  
+        }                                                                                                                
+        if(element.object.content === ''){
+            $.each(element.object.attachments, function(){
+                if(this.content !== undefined){                                    
+                    post.message = this.content;                                                                                                           
+                }
+                else if(this.displayName !== undefined){                                    
+                    post.message = this.displayName + '<br />' + this.url;
+                }                                
+            });                            
+        }
+        else{                            
+            post.message = element.object.content;                                                                               
+        }
+        post.social_network = 'google-plus';      
+        post.link = element.url;                                          
+        getTemplate(post, json.items[json.items.length-1] == element);                    
+    }); 
+} 
+
+});
+}
+function getVKData(account){
+    var vk_json = 'https://api.vk.com/method/wall.get?owner_id='+account+'&filter='+options.vk.source+'&count='+options.vk.limit+'&callback=?',
+    vk_user_json_template = 'https://api.vk.com/method/users.get?fields=first_name,%20last_name,%20screen_name,%20photo&uid=',
+    vk_group_json_template = 'https://api.vk.com/method/groups.getById?fields=first_name,%20last_name,%20screen_name,%20photo&gid=';
+    $.get(vk_json,function(json){
+        var vk_wall_owner =(account > 0) ? (vk_user_json_template + account + '&callback=?') : (vk_group_json_template+(-1) * account + '&callback=?'); 
+        $.get(vk_wall_owner,function(wall_owner){
+
+         $.each(json.response, function(){ 
+            if (this != parseInt(this)){
+                var element = this,
+                post = {};
+                    post.dt_create = moment.unix(element.date);//dateToSeconds(new Date(element.date*1000));
+                    post.description = ' ';
+                    post.message = stripHTML(element.text);
+                    if (options.show_media) {
+                        if (element.attachment){
+                            if (element.attachment.type=='video')
+                                post.attachment='<img class="attachment" src="'+element.attachment.video.image_big+'" />';
+                            if (element.attachment.type=='photo')
+                                post.attachment='<img class="attachment" src="'+element.attachment.photo.src_big+'" />';
+                            if (element.attachment.type=='link')
+                                post.attachment='<img class="attachment" src="'+element.attachment.link.image_src+'" />';
+                        }
+                    }
+                    post.social_network='vk';
+                    //if the post is created by user
+                    if (element.from_id > 0){
+                        var vk_user_json = vk_user_json_template + element.from_id + '&callback=?';
+                        $.get(vk_user_json,function(user_json){
+                            post.author_name = user_json.response[0].first_name + ' ' + user_json.response[0].last_name;
+                            post.author_picture = user_json.response[0].photo;
+                            post.author_link = 'http://vk.com/' + user_json.response[0].screen_name;
+                            post.link = 'http://vk.com/' + wall_owner.response[0].screen_name + '?w=wall' + element.to_id + '_' + element.id + '%2Fall';
+                            getTemplate(post, json.response[json.response.length-1] == element); 
+                        },'json');
+                        //if the post is created by group    
+                    }else{
+                        var vk_group_json = vk_group_json_template+(-1) * element.from_id + '&callback=?';
+                        $.get(vk_group_json,function(user_json){
+                            post.author_name = user_json.response[0].name;
+                            post.author_picture = user_json.response[0].photo;
+                            post.author_link = 'http://vk.com/' + user_json.response[0].screen_name;
+                            post.link = 'http://vk.com/' + wall_owner.response[0].screen_name + '?w=wall-' + user_json.response[0].gid  + '_' + element.id;
+                            getTemplate(post, json.response[json.response.length-1] == element);
+                        },'json');
+                    }      
+                }
+            });  
+},'json');
+},'json');
+}
+function getInstagramData(account) {
+    var feed_json, search = '', limit = 'count=' + options.instagram.limit, query_extention = 'client_id=' + options.instagram.client_id + '&' + limit + '&callback=?', igm_api = 'https://api.instagram.com/v1/';
+    igm_api = igm_api + 'users/' + account + '/media/recent?';
+    feed_json = igm_api + query_extention;
+    $.ajax({
+        url : feed_json,
+        dataType : 'jsonp',
+        success : function(json) {
+
+           $.each(json.data, function() {
+            var post = {}, element = this;
+            post.id = element.id;
+            post.dt_create = moment((element.created_time * 1000));
+            post.author_link = 'http://instagram.com/' + element.user.username;
+            post.author_picture = element.user.profile_picture;
+            post.post_url = element.link;
+            post.author_name = element.user.full_name;
+            post.message = element.caption.text;
+            post.description = '';
+            post.social_network = 'instagram';
+            post.link = element.link;
+            if (options.show_media) {
+                post.attachment = '<img class="attachment" src="' + element.images.standard_resolution.url + '' + '" />';
+            }
+            getTemplate(post, json.data[json.data.length-1] == element);
+        });
+       }
+   });
+
+}
+function getTwitterData(account){
+    var tw_json = 'http://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + account + '&count=' + options.tw_limit + '&callback=?';
+    $.ajax({
+        url:tw_json,
+        dataType:'json',
+        timeout:1000,
+        success:function(json){
+
+         $.each(json, function(i) {              
+            var post = {},
+            element = this;
+            post.dt_create = moment(fixTwitterDate(element.created_at));
+            post.author_link = 'http://twitter.com/'+element.user.screen_name;
+            post.author_picture = element.user.profile_image_url;
+            post.post_url = post.author_link + '/status/' + element.id_str;
+            post.author_name = element.user.name;
+            post.message = element.text;
+            post.description = '';
+            post.social_network = 'twitter';
+            post.link = 'http://twitter.com/' + element.user.screen_name + '/status/' + element.id_str;
+            if (options.cookies)
+                $.cookie('social-feed-twitter' + i,JSON.stringify(post));
+            getTemplate(post,json[json.length-1] == element);                    
+        });
+     }, 
+     error:function(e){
+        if (options.cookies){
+                        //if can not fetch data from Twitter (because of the 150 responses / hour limit) get them from cookies
+                        for (var i = 0, limit = options.tw_limit; i < limit && $.cookie('social-feed-twitter' + i) != null; i++){
+                            var data = JSON.parse($.cookie('social-feed-twitter' + i));
+                            data.dt_create = moment(data.dt_create);
+                            getTemplate(data);
+                        }            
+                    }
+                }
+            });
+}
+//---------------------------------------------------------------------------------
+//Render functions
+//---------------------------------------------------------------------------------
+function getTemplate(data, lastelement){
+    var content = data;     
+    content.attachment=(content.attachment==undefined) ? '' : content.attachment;
+    content.time_ago = data.dt_create.fromNow();
+    content.dt_create=content.dt_create.valueOf();
+    content.text = wrapLinks(shorten(data.message + ' ' + data.description),data.social_network);
+    if (template!=undefined)
+        placeTemplate(template(content),data, lastelement);  
+    else 
+        $.get(options.template,function(template_html){
+            template = doT.template(template_html);
+            placeTemplate(template(content),data,lastelement);      
+        });
+
+}
+function placeTemplate(template,data, lastelement){
+    if ($(container).children().length == 0){
+        $(container).append(template);  
+    }else{
+        var i = 0,
+        insert_index = -1;                    
+        $.each($(container).children(), function(){
+            if ($(this).attr('dt_create') < data.dt_create){
+                insert_index = i;
+                return false;
+            }
+            i++;
+        });
+        $(container).append(template);
+        if (insert_index >= 0){
+            insert_index++;
+            var before = $(container).children('div:nth-child('+insert_index+')'),
+            current = $(container).children('div:last-child');
+            $(current).insertBefore(before);  
+        }
+
+    }
+    if (lastelement){
+        loaded[data.social_network]--;
+        fireCallback();
+    }
+}
+//---------------------------------------------------------------------------------
+//Utility functions
+//---------------------------------------------------------------------------------
+function wrapLinks(string,social_network){
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;            
+    if (social_network === 'twitter'){
+        string = string.replace(/(@|#)([a-z0-9_]+)/ig, wrapTwitterTagTemplate);
+    }
+    else if(social_network === 'google-plus'){                
+        string = string.replace(/(@|#)([a-z0-9_]+['])/ig, wrapGoogleplusTagTemplate);                       
+    }
+    else{
+        string = string.replace(exp, wrapLinkTemplate);
+    }
+    return string;
+}
+function wrapLinkTemplate(string){
+    return '<a target="_blank" href="' + string + '">' + string + '<\/a>';
+}
+function wrapTwitterTagTemplate(string){
+    return '<a target="_blank" href="http://twitter.com/' + string + '" >' + string + '<\/a>';
+}
+function wrapGoogleplusTagTemplate(string){
+    return '<a target="_blank" href="https://plus.google.com/s/' + string + '" >' + string + '<\/a>';   
+}
+function fixTwitterDate(created_at) {
+    created_at = created_at.replace('+0000','Z');
+    if(created_at !== undefined)
+        return created_at;
+}
+function shorten(string){
+    string = $.trim(string);
+    if (string.length > options.length)
+    {
+        var cut = string.substring(0, options.length),
+        link_start_position = cut.lastIndexOf('http');
+        if (link_start_position > 0){
+            var link_end_position = string.indexOf(' ',link_start_position);
+            if (link_end_position > options.length && string != string.substring(0,link_end_position))
+                return string.substring(0,link_end_position) + " ..";
+            else
+                return string;
+        }else{
+            return cut + "..";
+        } 
+    }else
+    return string;
+}
+function stripHTML(string){
+    if (typeof string === "undefined" || string === null)
+        return '';
+    return string.replace(/(<([^>]+)>)|nbsp;|\s{2,}|/ig,"");
+}
+};  
+
+})(jQuery); 
